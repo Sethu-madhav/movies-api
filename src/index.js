737f388711f6b63fb158c2dcdca6bff05bace0a1
Sethu-madhav <sethu4784@gmail.com>
2024-07-19 20:20:05 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const routes = require("./routes");
+
+app.use(express.json());
+app.use("/api", routes);
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
